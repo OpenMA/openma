07a8077740bc9c0e484ab5cd0790687cfa9ea965
Arnaud Barré <arnaud.barre@gmail.com>
2016-04-06 19:28:29 -0400

    New unit tests for the class ma::body::PluginGait.

---
+++ modules/body/test/plugingaitTest.cpp
@@ -0,0 +1,37 @@
+#include <cxxtest/TestDrive.h>
+
+#include "plugingaitTest_def.h"
+
+CXXTEST_SUITE(PluginGaitTest)
+{   
+  CXXTEST_TEST(clone)
+  {
+    ma::body::PluginGait helper(ma::body::Region::Full, ma::body::Side::Both);
+    fill_parameters_with_fake_value(&helper);
+    ma::Node root("root");
+    auto clonedhelper = helper.clone(&root);
+    helper.setLeftWristWidth(24.0);
+    compare_parameters_with_fake_value(clonedhelper);
+  };
+  
+  CXXTEST_TEST(copy)
+  {
+    ma::body::PluginGait helper(ma::body::Region::Full, ma::body::Side::Both);
+    ma::body::PluginGait copyhelper(ma::body::Region::Lower, ma::body::Side::Right);
+    fill_parameters_with_fake_value(&helper);
+    copyhelper.setMarkerDiameter(16.0);
+    copyhelper.setHeadOffsetEnabled(false);
+    copyhelper.setLeftShoulderOffset(71.3);
+    copyhelper.setRightElbowWidth(50.1);
+    copyhelper.setLeftLegLength(942.0);
+    copyhelper.setRightAsisTrochanterAPDistance(123.5); 
+    copyhelper.setRightKneeWidth(93.0);
+    copyhelper.copy(&helper);
+    compare_parameters_with_fake_value(&helper);
+    compare_parameters_with_fake_value(&copyhelper);
+  };
+};
+
+CXXTEST_SUITE_REGISTRATION(PluginGaitTest)
+CXXTEST_TEST_REGISTRATION(PluginGaitTest, clone)
+CXXTEST_TEST_REGISTRATION(PluginGaitTest, copy)
