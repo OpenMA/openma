40e40d566acd3678d984a258e88e4d328f20faa5
Arnaud Barré <arnaud.barre@gmail.com>
2016-04-13 09:12:21 -0400

    The function c3dhandlertest_sample01 was also in the file ioTest.cpp.

--- modules/io/test/ioTest.cpp
+++ modules/io/test/ioTest.cpp
@@ -10,14 +10,14 @@ CXXTEST_SUITE(IoTest)
   CXXTEST_TEST(readOne)
   {
     auto root = ma::io::read(OPENMA_TDD_PATH_IN("c3d/standard/sample01/Eb015pi.c3d"));
-    c3dhandlertest_sample01("PI", "Eb015pi.c3d", root);
+    c3dhandlertest_read_sample01("PI", "Eb015pi.c3d", root);
   };
   
   CXXTEST_TEST(readTwo)
   {
     ma::Node root("root");
     TS_ASSERT_EQUALS(ma::io::read(&root, OPENMA_TDD_PATH_IN("c3d/standard/sample01/Eb015pi.c3d")), true);
-    c3dhandlertest_sample01("PI", "Eb015pi.c3d", &root);
+    c3dhandlertest_read_sample01("PI", "Eb015pi.c3d", &root);
   };
 };
 
