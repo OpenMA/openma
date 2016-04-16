1a06a76339851c7d95e82f3be0a4cc577cbac85a
Arnaud Barré <arnaud.barre@gmail.com>
2016-04-16 07:32:41 -0400

    External library required to uncompress data in the QTM file format.

---
+++ 3rdparty/lzo1xdec/test/lzo1xdecTest.cpp
@@ -0,0 +1,23 @@
+#include <cxxtest/TestDrive.h>
+#include <lzo1xdec/lzo1xdec.h>
+#include <cstring> // memcmp
+
+const unsigned char _lzo_test_encoded[40] = {0x00, 0x0D, 0x54, 0x68, 0x69, 0x73, 0x20, 0x69, 0x73, 0x20, 0x6D, 0x79, 0x20, 0x74, 0x65, 0x73, 0x74, 0x20, 0x74, 0x65, 0x78, 0x74, 0x20, 0x66, 0x69, 0x6C, 0x65, 0x2E, 0x0A, 0x54, 0x68, 0x69, 0x73, 0x20, 0x7D, 0x68, 0x00, 0x11, 0x00, 0x00};
+const char _lzo_test_decoded[] = "This is my test text file.\nThis is my test text file.\nThis is my test text file.\nThis is my test text file.\nThis is my test text file.\nThis is my test text file.\nThis is my test text file.\n";
+
+CXXTEST_SUITE(LZO1xDecTest)
+{
+  CXXTEST_TEST(decode)
+  {
+    unsigned char out[189] = {0};
+    int inlen = sizeof(_lzo_test_encoded);
+    int outlen = sizeof(out);
+    int remlen = outlen;
+    ::lzo1x_decode(out, &remlen, _lzo_test_encoded, &inlen);
+    TS_ASSERT_EQUALS(remlen, 0); // All the buffer was used
+    TS_ASSERT_EQUALS(memcmp(out, _lzo_test_decoded, outlen), 0);
+  };
+};
+
+CXXTEST_SUITE_REGISTRATION(LZO1xDecTest)
+CXXTEST_TEST_REGISTRATION(LZO1xDecTest, decode)
