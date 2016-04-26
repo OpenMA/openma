3da167e017387a832711545cc593eafe4864f1c3
Arnaud Barré <arnaud.barre@gmail.com>
2016-04-26 09:58:11 -0400

    New unit tests for the Python class ma.Node.

--- modules/base/test/python/nodeTest.py
+++ modules/base/test/python/nodeTest.py
@@ -5,3 +5,17 @@ class NodeTest(unittest.TestCase):
     def test_ctor(self):
         n = ma.Node('test')
         self.assertEqual(n.name(), 'test')
+        
+    def test_ctor_with_parent(self):
+        root = ma.Node('root')
+        child = ma.Node('child', root)
+        self.assertEqual(root.hasChildren(), True)
+        self.assertEqual(child.hasParents(), True)
+        self.assertEqual(root.name(), 'root')
+        self.assertEqual(child.name(), 'child')
+        del root
+        self.assertEqual(child.hasParents(), False)
+    
+    def test_child0(self):
+        root = ma.Node('root');
+        self.assertRaises(IndexError, root.child, 0);
