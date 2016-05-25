ca0ee2aea49025b9757e2d1ab499955cef611c3d
Arnaud Barré <arnaud.barre@gmail.com>
2016-05-25 19:11:36 -0400

    Test for the force platform Type-4.

---
+++ modules/instrument/test/forceplatetype4Test.cpp
@@ -0,0 +1,18 @@
+#include <cxxtest/TestDrive.h>
+
+#include "forceplateTest_def.h"
+
+#include <openma/instrument/forceplatetype4.h>
+
+CXXTEST_SUITE(ForcePlateType4Test)
+{
+  CXXTEST_TEST(wrench)
+  {
+    ma::instrument::ForcePlateType4 fp("FP");
+    forceplatetest_fill_sample10_type4(&fp);
+    forceplatetest_compare_wrench_at_origin(&fp, fp4dataout);
+  };
+};
+
+CXXTEST_SUITE_REGISTRATION(ForcePlateType4Test)
+CXXTEST_TEST_REGISTRATION(ForcePlateType4Test, wrench)
