51de8f5767734e8b27883571901c2a2d5ebfff39
Arnaud Barré <arnaud.barre@gmail.com>
2016-05-27 23:33:07 -0400

    Name of the template parameter changed.

--- modules/math/include/openma/math/forwarddeclarations.h
+++ modules/math/include/openma/math/forwarddeclarations.h
@@ -61,7 +61,7 @@ namespace math
   template <typename XprOne, typename XprTwo> class CrossOp;
   template <typename Xpr> class TransposeOp;
   template <typename XprOne, typename XprTwo> class TransformOp;
-  template <typename U> class ReplicateOp;
+  template <typename Xpr> class ReplicateOp;
   template <typename Xpr> class InverseOp;
   template <typename Xpr> class EulerAnglesOp;
   template <typename Xpr, unsigned U> class DerivativeOp;
