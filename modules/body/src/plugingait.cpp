f46ce4bcfa9da6abe2e63f27efd9eafd654c3186
Arnaud Barré <arnaud.barre@gmail.com>
2016-09-24 21:49:46 -0400

    Missing computation for the pelvis length.

--- modules/body/src/plugingait.cpp
+++ modules/body/src/plugingait.cpp
@@ -1089,6 +1089,9 @@ namespace body
       const math::Vector v = (L_ASIS - R_ASIS).normalized();
       const math::Vector w = ((R_ASIS - SC).cross(L_ASIS - SC)).normalized();
       const math::Pose pelvis(v.cross(w), v, w, (L_ASIS + R_ASIS) / 2.0);
+      // Set the segment length
+      // NOTE: the coefficient 0.828 comes from Vicon
+      this->setProperty("Pelvis.length", 0.828 * (_L_HJC - _R_HJC).matrix().norm());
       // -----------------------------------------
       // Other lower limbs (dependant of the hip joint centres)
       // -----------------------------------------
@@ -1259,6 +1262,7 @@ namespace body
       const math::Vector w = ((R_ASIS - SC).cross(L_ASIS - SC)).normalized();
       const math::Pose pelvis(v.cross(w), v, w, (L_ASIS + R_ASIS) / 2.0);
       math::to_timesequence(pelvis, seg->name()+".TCS", sampleRate, startTime, TimeSequence::Pose, "", seg);
+      seg->setProperty("length", this->property(seg->name()+".length"));
       pelvisSCS = math::to_timesequence(transform_relative_frame(relframe, seg, pelvis), seg->name()+".SCS", sampleRate, startTime, TimeSequence::Pose, "", relframe);
       // -----------------------------------------
       // Thigh, shank, foot
