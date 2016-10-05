42790d756f5163fc4db5f82c3e68900875dd536c
Arnaud Barré <arnaud.barre@gmail.com>
2016-10-05 14:45:46 -0400

    New Python unit test for the IO module.

--- modules/io/test/python/ioTest.py
+++ modules/io/test/python/ioTest.py
@@ -1,7 +1,8 @@
 import ma.io
 import ma.instrument
 import unittest
-from test_file_path import openma_tdd_path_in
+import numpy as np
+from test_file_path import openma_tdd_path_in, openma_tdd_path_out
 
 class IOTest(unittest.TestCase):
     def test_wrong_path(self):
@@ -29,3 +30,15 @@ class IOTest(unittest.TestCase):
         self.assertAlmostEqual(data[16,0], 285.0, 3)
         self.assertAlmostEqual(data[16,1], 222.4166, 3)
         self.assertAlmostEqual(data[16,2], 564.4166, 3)
+        
+    def test_write_c3d_from_scratch(self):
+        root = ma.Node('root')
+        trial = ma.Trial('my_analysis',root)
+        trial.setProperty('MY_GROUP:MY_PARAMETER',10.0)
+        angle = ma.TimeSequence('LHipAngles.Left', 4, 101, 1.0, 0.0, ma.TimeSequence.Type_Angle, 'deg', trial.timeSequences())
+        angle.setData(np.random.rand(101,4))
+        self.assertEqual(ma.io.write(root,openma_tdd_path_out('c3d/my_analysis.c3d')),True)
+        root_ = ma.io.read(openma_tdd_path_out('c3d/my_analysis.c3d'))
+        self.assertEqual(len(root_.children()),1)
+        self.assertEqual(root_.child(0).property('MY_GROUP:MY_PARAMETER').cast(), 10.0)
+        self.assertNotEqual(root_.child(0).property('POINT:SCALE').cast(), 0.0)
